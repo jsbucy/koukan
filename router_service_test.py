jsbucy/koukan

commit 7d22381a2c300bf9264ad3fd09535b36d42620b0

- add inflight waiting to RouterServiceTransaction.get   -- storage wait_attr_not_none early-return if status not inflight - router service support manually invoking dequeue/gc in tests,   router_service_test do this to eliminiate a lot of flaky sleeping

rest_endpoint.py
@@ -54,6 +54,7 @@ class BlobIdMap:
             self.cv.notify_all()
 
 class RestEndpoint:
+    # XXX I don't remember exactly why this is cached like this
     start_resp : Optional[Response] = None
     final_status : Optional[Response] = None
     transaction_url : Optional[str] = None
@@ -87,23 +88,30 @@ class RestEndpoint:
         self.wait_response = wait_response
 
     def start(self,
-              local_host, remote_host,
-              mail_from, transaction_esmtp,
-              rcpt_to, rcpt_esmtp):
+              local_host=None, remote_host=None,
+              mail_from=None, transaction_esmtp=None,
+              rcpt_to=None, rcpt_esmtp=None):
         next_hop = (self.static_remote_host if self.static_remote_host
                     else remote_host)
-        req_json = {
-            'local_host': local_host,
-            'remote_host': next_hop,
-            'mail_from': mail_from,
-            'rcpt_to': rcpt_to
-        }
+        req_json = {}
+        if local_host is not None:
+            req_json['local_host'] = local_host
+        if remote_host is not None:
+            req_json['remote_host'] = remote_host
+        if mail_from is not None:
+            req_json['mail_from'] = mail_from
+        if rcpt_to is not None:
+            req_json['rcpt_to'] = rcpt_to
+        # xxx esmtp
+
         rest_resp = requests.post(self.base_url + '/transactions',
                                   json=req_json,
                                   headers={'host': self.http_host},
                                   timeout=self.timeout_start)
-        print(rest_resp)
+        logging.info('RestEndpoint.start resp %s', rest_resp)
         resp_json = get_resp_json(rest_resp)
+        logging.info('RestEndpoint.start resp_json %s', resp_json)
+
         # XXX  rest_resp.status_code or 'start_response' in resp_json
         if not resp_json or 'url' not in resp_json:
             return Response.Internal(
@@ -250,11 +258,11 @@ class RestEndpoint:
 
         return Response(), dlen
 
-    def start_response(self, timeout):
+    def start_response(self, timeout) -> Optional[Response]:
         if self.start_resp: return self.start_resp
         if not self.wait_response: return None
         # TODO inflight waiter list thing?
-        resp = self.get_json_field(timeout, 'start_response')
+        resp = self.get_json_response(timeout, 'start_response')
         if not resp:
             return Response(400, 'RestEndpoint.start_response timeout')
         self.start_resp = resp
@@ -264,29 +272,28 @@ class RestEndpoint:
         if self.final_status: return self.final_status
         if not self.wait_response: return None
         # TODO inflight waiter list thing?
-        resp = self.get_json_field(timeout, 'final_status')
+        resp = self.get_json_response(timeout, 'final_status')
         if not resp:
             return Response(400, 'RestEndpoint.get_status timeout')
         self.final_status = resp
         return self.final_status
 
-    def resp_from_rest_resp(self, rest_resp, field):
-        if rest_resp.status_code >= 500:
-            return None
-        if not (resp_json := get_resp_json(rest_resp)):
-            return None
-        if field not in resp_json or not resp_json[field]:
-            return None
-        return Response.from_json(resp_json[field])
+
+    # XXX clearly distinguish internal, test methods
 
     def get_json(self, timeout):
-        rest_resp = requests.get(self.transaction_url,
-                                 headers={'host': self.http_host},
-                                 timeout=timeout)
+        try:
+            rest_resp = requests.get(self.transaction_url,
+                                     headers={'host': self.http_host},
+                                     timeout=timeout)
+        except Exception:
+            return None
         return get_resp_json(rest_resp)
 
-    # block until transaction json contains field and is non-null or timeout
-    def get_json_field(self, timeout, field):
+
+    # block until transaction json contains field and is non-null or timeout,
+    # returns Response.from_json() from that field
+    def get_json_response(self, timeout, field) -> Optional[Response]:
         now = time.monotonic()
         deadline = now + timeout
         delta = 1
@@ -297,28 +304,23 @@ class RestEndpoint:
                 if deadline_left < 1:
                     break
                 time.sleep(min(1 - delta, deadline_left))
-            logging.info('RestEndpoint.get_json %f', deadline_left)
+            logging.info('RestEndpoint.get_json_response %f', deadline_left)
             start = time.monotonic()
-            # XXX throws on timeout
+
             # TODO pass the deadline in a header?
-            rest_resp = requests.get(self.transaction_url,
-                                     headers={'host': self.http_host},
-                                     timeout=deadline_left)
-            logging.info('RestEndpoint.get_json done %s', rest_resp)
+            resp_json = self.get_json(deadline_left)
+            logging.info('RestEndpoint.get_json_response done %s', resp_json)
             delta = time.monotonic() - start
             assert(delta >= 0)
-            if rest_resp.status_code >= 400 and rest_resp.status_code < 500:
+            if resp_json is None:
                 return Response(400, 'RestEndpoint.start_response GET '
                                 'failed')
-            resp_json = get_resp_json(rest_resp)
-            logging.info('RestEndpoint.get_json %s', resp_json)
-            if (resp := self.resp_from_rest_resp(rest_resp, field)) is not None:
-                return resp
+
+            if field in resp_json:
+                return Response.from_json(resp_json[field])
             now = time.monotonic()
         return None
 
-    def wait_status(self):
-        pass
 
     def set_durable(self):
         # XXX appended_last only gets set after the append has

router_service.py
@@ -115,9 +115,16 @@ class Service:
     lock : Lock = None
     inflight : Dict[str, RouterTransaction] = None
     last_gc = 0
-    stop_dequeue = False
 
     rest_tx_factory : RestServiceTransactionFactory = None
+
+    # dequeue watermark
+    created_id : Optional[int] = None
+
+    dequeue_thread : Optional[Thread] = None
+    gc_thread : Optional[Thread] = None
+
+    _shutdown = False
 
     def __init__(self, config=None):
         self.lock = Lock()
@@ -143,8 +150,13 @@ class Service:
 
     def shutdown(self):
         logging.info("router_service shutdown()")
-        self.stop_dequeue = True
-        self.dequeue_thread.join()
+        self._shutdown = True
+        if self.dequeue_thread is not None:
+            self.dequeue_thread.join()
+        if self.gc_thread is not None:
+            self.gc_thread.join()
+
+        assert(self.executor.wait_empty(timeout=5))
 
         with self.lock:
             txx = [tx for id,tx in self.inflight.items()]
@@ -165,7 +177,7 @@ class Service:
 
         db_filename = self.config.get_str('db_filename')
         if not db_filename:
-            print("*** using in-memory/non-durable storage")
+            logging.warning("*** using in-memory/non-durable storage")
             self.storage.connect(db=Storage.get_inmemory_for_test())
         else:
             self.storage.connect(filename=db_filename)
@@ -173,9 +185,18 @@ class Service:
 
         self.blobs = BlobStorage()
 
-        self.dequeue_thread = Thread(target = lambda: self.load(),
-                                     daemon=True)
-        self.dequeue_thread.start()
+        if self.config.get_bool('dequeue', True):
+            self.dequeue_thread = Thread(target = lambda: self.dequeue(),
+                                         daemon=True)
+            self.dequeue_thread.start()
+
+        if self.config.get_int('gc_interval') > 0:
+            self.gc_thread = Thread(target = lambda: self.gc(),
+                                    daemon=True)
+            self.gc_thread.start()
+        else:
+            logging.warning('idle gc disabled')
+
 
         # top-level: http host -> endpoint
 
@@ -238,44 +259,58 @@ class Service:
     def handle_tx(self, storage_tx : TransactionCursor, endpoint : object):
         cursor_to_endpoint(storage_tx, endpoint)
 
-    def load(self):
-        created_id = None
-        while not self.stop_dequeue:
-            logging.info("dequeue %s", self.stop_dequeue)
-            storage_tx = None
-            if self.rest_tx_factory is not None:
-                if self.storage.wait_created(created_id, timeout=1):
-                    storage_tx = self.storage.load_one()
-                if (storage_tx is not None and
-                    storage_tx.status == Status.INSERT):  # XXX status??
-                    created_id = storage_tx.id
+    def _dequeue(self) -> bool:
+        storage_tx = None
+        if self.rest_tx_factory is not None:
+            if self.storage.wait_created(self.created_id, timeout=1):
+                # XXX this spins if for whatever reason we didn't load
+                # anything -> need to advance the watermark
+                storage_tx = self.storage.load_one()
+                if storage_tx is not None:
+                    self.created_id = storage_tx.id
 
-            self._gc_inflight()
+        tag = None
+        # XXX config, backoff
+        # XXX there is a race between RouterTransaction updating
+        # the storage (which makes it selectable here) vs running
+        # the done callback, if min_age is too low, this can clash
+        # with inflight
+        if storage_tx is None:
+            # XXX this will load INSERT but we probably only want WAITING?
+            storage_tx = self.storage.load_one(
+                min_age=self.config.get_int('retry_min_age', 5))
+            tag = Tag.LOAD
+        if storage_tx is None:
+            return False
 
-            tag = None
-            # XXX config, backoff
-            # XXX there is a race between RouterTransaction updating
-            # the storage (which makes it selectable here) vs running
-            # the done callback, if min_age is too low, this can clash
-            # with inflight
-            if storage_tx is None:
-                # XXX this will load INSERT but we probably only want WAITING?
-                storage_tx = self.storage.load_one(min_age=5)
-                tag = Tag.LOAD
-            if storage_tx:
-                logging.info("dequeued %d", storage_tx.id)
-                if self.rest_tx_factory:
-                    endpoint, msa = self.wiring.get_endpoint(storage_tx.host)
-                    assert(endpoint is not None)
-                    tag = Tag.MSA if msa else Tag.MX
-                    self.executor.enqueue(
-                        tag, lambda: self.handle_tx(storage_tx, endpoint))
-                else:
-                    self.executor.enqueue(tag, lambda: self.handle(storage_tx))
-            else:
-                logging.info("dequeue idle")
+        logging.info("dequeued %d", storage_tx.id)
+        if self.rest_tx_factory:
+            # XXX there is a race that this can be selected
+            # between creation and writing the envelope
+            storage_tx.wait_attr_not_none('host')
+            endpoint, msa = self.wiring.get_endpoint(storage_tx.host)
+            tag = Tag.MSA if msa else Tag.MX
+            self.executor.enqueue(
+                tag, lambda: self.handle_tx(storage_tx, endpoint))
+        else:
+            self.executor.enqueue(tag, lambda: self.handle(storage_tx))
+        return True
+
+    def dequeue(self):
+        while not self._shutdown:
+            logging.info("RouterService.dequeue")
+            if not self._dequeue():
                 # xxx config
-                time.sleep(1)
+                # otherwise we block in wait_created
+                if self.rest_tx_factory is None:
+                    logging.info("dequeue idle")
+                    time.sleep(1)
+
+    def gc(self):
+        while not self._shutdown:
+            self._gc_inflight(self.config.get_int('tx_idle_timeout', 5))
+            # xxx wait for shutdown
+            time.sleep(self.config.get_int('gc_interval'))
 
     def _add_inflight(self, tx):
         assert(tx.rest_id)
@@ -288,15 +323,15 @@ class Service:
         with self.lock:
             del self.inflight[tx.rest_id]
 
-    def _gc_inflight(self):
+    def _gc_inflight(self, idle_timeout=None):
         now = time.monotonic()
-        if (now - self.last_gc) < self.config.get_int('tx_idle_gc', 5): return
-        logging.info('router_service _gc_inflight')
-        self.last_gc = now
+        logging.info('router_service _gc_inflight %d', idle_timeout)
 
-        idle_timeout = self.config.get_int('tx_idle_timeout', 5)
         if self.rest_tx_factory:
-            self.storage.gc_non_durable(idle_timeout)
+            count = self.storage.gc_non_durable(idle_timeout)
+            logging.info('router_service _gc_inflight aborted %d', count)
+            return count
+
         with self.lock:
             dele = []
             for rest_id, tx in self.inflight.items():

commit 5c2bc417098ebedeae094abec0021f175a95f937

- remove INFLIGHT_DONE status, it's just DONE - clarify that set_durable can only succeed/noop if the transaction   succeeded, otherwise is a precondition failure - the above makes router_service_test gc inflight work - fix idiotic rest_endpoint regression populating remote_host from   7d22381a2c300bf9264ad3fd09535b36d42620b0, this desperately needs a   unittest! - Config return default if json isn't the desired type

rest_endpoint.py
@@ -60,6 +60,7 @@ class RestEndpoint:
     transaction_url : Optional[str] = None
     blob_id_map : BlobIdMap = None
     appended_last = False
+    static_remote_host : Optional[str] = None
 
     # static_remote_host overrides transaction remote_host to send all
     # traffic to a fixed next-hop
@@ -96,8 +97,8 @@ class RestEndpoint:
         req_json = {}
         if local_host is not None:
             req_json['local_host'] = local_host
-        if remote_host is not None:
-            req_json['remote_host'] = remote_host
+        if next_hop is not None:
+            req_json['remote_host'] = next_hop
         if mail_from is not None:
             req_json['mail_from'] = mail_from
         if rcpt_to is not None:

router_service.py
@@ -76,7 +76,7 @@ class ShimTransaction:
         if not storage_tx.load(rest_id=rest_id):
             return False
         status = storage_tx.status
-        if status != Status.DONE and status != Status.ONESHOT_DONE:
+        if status != Status.DONE:
             return True
 
         actions = storage_tx.load_last_action(1)
@@ -90,16 +90,14 @@ class ShimTransaction:
         # or PERM_FAIL, sync mx can be TEMP_FAIL. PERM_FAIL
         # ~corresponds to whether we would send a bounce
 
-        if (action == Action.TEMP_FAIL and
-            status != Status.ONESHOT_DONE):
+        if action == Action.TEMP_FAIL:
             pass
         else:
             self.final_status = response
 
-        logging.info('ShimTransaction.read %s status=%d action=%d', rest_id, status, action)
+        logging.info('ShimTransaction.read %s status=%d action=%d',
+                     rest_id, status, action)
         if status == Status.DONE:
-            self.done = True
-        elif status == Status.ONESHOT_DONE and action != Action.TEMP_FAIL:
             self.done = True
 
         return True
@@ -190,7 +188,7 @@ class Service:
                                          daemon=True)
             self.dequeue_thread.start()
 
-        if self.config.get_int('gc_interval') > 0:
+        if self.config.get_int('gc_interval') is not None:
             self.gc_thread = Thread(target = lambda: self.gc(),
                                     daemon=True)
             self.gc_thread.start()

smtp_endpoint.py
@@ -30,14 +30,14 @@ class Factory:
 
 # rest or smtp services call upon a single instance of this
 class SmtpEndpoint:
-    smtp : smtplib.SMTP
-    data : bytes = None
-    start_resp : Response = None
-    final_status : Response = None
-    received_last = False  # xxx rest service endpoint abc
-    blobs_received = -1
-    rest_id = None
-    idle_start = None
+    smtp : Optional[smtplib.SMTP] = None
+    data : Optional[bytes] = None
+    start_resp : Optional[Response] = None
+    final_status : Optional[Response] = None
+    received_last: bool  = False  # xxx rest service endpoint abc
+    blobs_received : int = -1
+    rest_id : Optional[str] = None
+    idle_start : Optional[float] = None
 
     def __init__(self, rest_id, ehlo_hostname):
         self.rest_id = rest_id
@@ -73,7 +73,7 @@ class SmtpEndpoint:
 
 
     def start(self,
-              local_host, remote_host,
+              local_host, remote_host : Tuple[str, int],
               mail_from, transaction_esmtp,
               rcpt_to, rcpt_esmtp=None):
         self.idle_start = time.monotonic()

storage.py
@@ -18,13 +18,6 @@ class Status:
     WAITING = 1
     INFLIGHT = 2
     DONE = 3
-    # XXX I think ONESHOT_DONE should go away:
-    # oneshot_inflight -> set durable -> inflight
-    # oneshot_temp -> set durable -> waiting
-    # done -> set durable -> done (noop)
-    # insert -> abort -> done
-    # oneshot inflight -> abort -> done
-    ONESHOT_DONE = 4  # set_durable noop
     ONESHOT_INFLIGHT = 5
     ONESHOT_TEMP = 6  # set_durable -> WAITING
 
@@ -47,29 +40,8 @@ class Action:
     # START is basically upstream start succeeded, upstream start err ->
     # overall tx "attempt" result
     START = 7  # inflight -> inflight (no change)
-    SET_DURABLE = 8  # ONESHOT_DONE -> WAITING or
+    SET_DURABLE = 8  # INSERT -> WAITING
                      # ONESHOT_INFLIGHT -> INFLIGHT
-
-# TransactionCursor:
-# sync success/permfail (single mx temp) -> oneshot
-#   create
-#   write envelope
-#   append action(resp)
-#     status ONESHOT_DONE
-#     action DELIVERED | PERM_FAIL
-
-# durable (msa tempfail, multi-mx mixed)
-#   create
-#   write envelope
-#   append blob
-#   append action
-#     status DONE | WAITING
-#     action DELIVERED | TEMP_FAIL | PERM_FAIL
-
-# retry
-#   load
-#   read blobs
-#   append action
 
 class TransactionCursor:
     # XXX need a transaction/request object or something
@@ -185,7 +157,6 @@ class TransactionCursor:
             assert(status in [Status.INSERT,
                               Status.INFLIGHT,  # xxx
                               Status.ONESHOT_INFLIGHT,
-                              Status.ONESHOT_DONE,
                               Status.ONESHOT_TEMP])  # e.g. start tempfailed upstream
             #xxx output side could have appended START action, etc.
             #assert(version == self.version)
@@ -219,7 +190,6 @@ class TransactionCursor:
             row = cursor.fetchone()
             status,version,db_last = row
             assert(status in [Status.INSERT,
-                              Status.ONESHOT_DONE,
                               Status.INFLIGHT,    # xxx
                               Status.ONESHOT_INFLIGHT ])
             logging.debug('Storage.append_blob version id=%d db %d new %d '
@@ -410,12 +380,12 @@ class TransactionCursor:
                 if action in [Action.DELIVERED,
                               Action.PERM_FAIL,
                               Action.ABORT]:
-                    status = Status.ONESHOT_DONE
+                    status = Status.DONE
                 elif action == Action.TEMP_FAIL:
                     status = Status.ONESHOT_TEMP
                 elif action == Action.START:
                     # no change
-                    assert(status in [Status.INFLIGHT, Status.ONESHOT_INFLIGHT])
+                    pass
                 elif action == Action.SET_DURABLE:
                     status = Status.INFLIGHT
                 else:
@@ -430,17 +400,10 @@ class TransactionCursor:
             elif status == Status.WAITING:
                 assert(action == Action.LOAD)
             elif status == Status.DONE:
-                # XXX ambiguous, at least 2 possibilities:
-                # 1: tx was aborted
-                # 2: previous set_durable succeeded but resp timed out
-                #    and this is a retry
-                # for the sake of the tests for the time being,
-                # assume #1, probably we need a separate
-                # Status.ABORTED or this needs to read the last action
-                # and error out if it was Action.ABORT
+                # this could be a noop if it already succeeded
+                # upstream but treat as failed precondition at storage
+                # layer, deal with that in the handler
                 return False
-            elif status == Status.ONESHOT_DONE:
-                assert(action == Action.SET_DURABLE)
             elif status == Status.INFLIGHT:
                 if action in [Action.DELIVERED,
                               Action.PERM_FAIL,
@@ -485,7 +448,7 @@ class TransactionCursor:
                 '  ?,?' + resp_bind + ')',
                 (self.id, self.id, now, action) + resp_val)
 
-            # TODO gc payload on status DONE (still need ONESHOT_DONE?)
+            # TODO gc payload on status DONE
             # (or ttl, keep for ~1d after DONE?)
             self.parent.db.commit()
             self.version = new_version

transaction.py
@@ -279,7 +279,26 @@ class RestServiceTransaction(Handler):
     def set_durable(self, req_json : Dict[str, Any]) -> FlaskResponse:
         if self.tx_cursor.append_action(Action.SET_DURABLE):
             return FlaskResponse()
-        return FlaskResponse(status=400, response=['failed precondition'])
+
+        if self.tx_cursor.status != Status.DONE:
+            return FlaskResponse(status=400, response=['failed precondition'])
+
+        # set durable in done state means that it raced with something
+        # else that terminated the transaction either upstream
+        # success/perm or idle gc/abort
+
+        # set durable succeeding is us "taking responsibility for the
+        # message" wrt rfc5321 so we can only treat this as a noop if
+        # it already succeeded
+
+        actions = self.tx_cursor.load_last_action(1)
+
+        if actions[0][1] != Action.DELIVERED:
+            return FlaskResponse(status=400, response=['failed precondition'])
+
+        return FlaskResponse()
+
+
 
 # interface to top-level flask app
 class RestServiceTransactionFactory(HandlerFactory):

router_service_test.py
@@ -44,7 +44,7 @@ config_json = {
     'db_filename': '',
     'use_gunicorn': False,
     'tx_idle_timeout': 5,
-    'gc_interval': 0,
+    'gc_interval': None,
     'dequeue': False,
 }
 
@@ -125,10 +125,12 @@ class RouterServiceTest(unittest.TestCase):
         self.assertNotIn('start_response', resp_json)
         self.assertNotIn('final_status', resp_json)
 
-        self.assertEqual(1, self.service._dequeue())
 
         # set start response, initial inflight tempfails
         self.endpoint.set_start_response(Response(456))
+
+        self.assertEqual(1, self.service._dequeue())
+
         resp = read_endpoint.get_json_response(3, 'final_status')
         logging.info('test_read_routing start resp %s', resp)
         self.assertIsNotNone(resp)
@@ -147,7 +149,7 @@ class RouterServiceTest(unittest.TestCase):
 
 
     # set durable after upstream ok/perm -> noop
-    def test_durable_after_upstream_done(self):
+    def test_durable_after_upstream_success(self):
         start_endpoint = RestEndpoint(
             base_url=self.router_url, http_host='outbound-gw',
             wait_response=False,
@@ -166,8 +168,30 @@ class RouterServiceTest(unittest.TestCase):
         self.assertIsNotNone(resp)
         self.assertEqual(256, resp.code)
 
-        # ONESHOT_DONE/DELIVERED -> noop
+        # DONE/DELIVERED -> noop
         self.assertTrue(start_endpoint.set_durable().ok())
+
+    def test_durable_after_upstream_perm(self):
+        start_endpoint = RestEndpoint(
+            base_url=self.router_url, http_host='outbound-gw',
+            wait_response=False,
+            msa=True)
+
+        self.endpoint.set_start_response(Response(234))
+        self.endpoint.add_data_response(Response(556))
+
+        start_resp = start_endpoint.start(mail_from='alice', rcpt_to='bob')
+        start_endpoint.append_data(last=True, blob=InlineBlob(b'hello'))
+
+        self.assertEqual(1, self.service._dequeue())
+
+        resp = start_endpoint.get_json_response(3, 'final_status')
+        logging.info('test_durable_after_upstream_done resp %s', resp)
+        self.assertIsNotNone(resp)
+        self.assertEqual(556, resp.code)
+
+        # DONE/PERMFAIL -> failed precondition
+        self.assertFalse(start_endpoint.set_durable().ok())
 
 
     def test_durable_after_upstream_temp(self):
@@ -249,7 +273,7 @@ class RouterServiceTest(unittest.TestCase):
     # xxx this only aborts insert/oneshot_temp, not inflight
     # do you want this to be the thing that makes output abort or
     # should it time out itself?
-    def disabled_test_idle_gc_oneshot_inflight(self):
+    def test_idle_gc_oneshot_inflight(self):
         start_endpoint = RestEndpoint(
             base_url=self.router_url, http_host='outbound-gw',
             wait_response=False,