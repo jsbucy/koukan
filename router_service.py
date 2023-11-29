4a2d1da56932b7f82e91dc7d64a6c1127f810953
John Bucy <bucy@gloop.org>
2023-11-29 13:55:00 -0800

    delete RouterTransaction

--- router_service.py
+++ router_service.py
@@ -14,7 +14,6 @@ from router import Router
 from rest_endpoint import RestEndpoint, BlobIdMap as RestBlobIdMap
 from dkim_endpoint import DkimEndpoint
 
-from router_transaction import RouterTransaction, BlobIdMap
 from storage import Storage, Action, Status, TransactionCursor
 
 from transaction import RestServiceTransactionFactory, cursor_to_endpoint
@@ -39,79 +38,9 @@ from wsgiref.simple_server import make_server
 
 from config import Config
 
-class RouterTransactionFactory(EndpointFactory):
-    def __init__(self, parent):
-        self.parent = parent
-    def create(self, host):
-        return self.parent.get_router_transaction(host)
-    def get(self, rest_id):
-        if rest_id in self.parent.inflight:
-            return self.parent.inflight[rest_id]
-        shim = ShimTransaction(self.parent)
-        if not shim.read(rest_id):
-            logging.info('RouterTransactionFactory.get shim read failed')
-            return None
-        return shim
-
-# stand-in for RouterTransaction for rest reads of at-rest/stored transactions
-class ShimTransaction:
-    received_last = True  # XXX
-    rest_id = None
-    done = False
-
-    def __init__(self, parent):
-        self.parent = parent
-        self.final_status = None
-
-    def get_start_result(self):
-        return None
-
-    def get_final_status(self, timeout=None):
-        return self.final_status
-
-    def read(self, rest_id):
-        logging.info('ShimTransaction.read %s', rest_id)
-        self.rest_id = rest_id
-        storage_tx = self.parent.storage.get_transaction_cursor()
-        if not storage_tx.load(rest_id=rest_id):
-            return False
-        status = storage_tx.status
-        if status != Status.DONE:
-            return True
-
-        actions = storage_tx.load_last_action(1)
-        if len(actions) != 1:
-            # more like INTERNAL, integrity problem if there aren't any
-            return True
-        time, action, response = actions[0]
-
-        # The semantics of final_status is that it's present iff we
-        # are done with it. async (msa/multi-mx) can only be DELIVERED
-        # or PERM_FAIL, sync mx can be TEMP_FAIL. PERM_FAIL
-        # ~corresponds to whether we would send a bounce
-
-        if action == Action.TEMP_FAIL:
-            pass
-        else:
-            self.final_status = response
-
-        logging.info('ShimTransaction.read %s status=%d action=%d',
-                     rest_id, status, action)
-        if status == Status.DONE:
-            self.done = True
-
-        return True
-
-    def set_durable(self):
-        logging.info('ShimTransaction.set_durable %s done=%s',
-                     self.rest_id, self.done)
-        if self.done:
-            return Response()  # no op
-        return Response(500, 'failed precondition')
 
 class Service:
     lock : Lock = None
-    inflight : Dict[str, RouterTransaction] = None
     last_gc = 0
 
     rest_tx_factory : RestServiceTransactionFactory = None
@@ -126,7 +55,6 @@ class Service:
 
     def __init__(self, config=None):
         self.lock = Lock()
-        self.inflight = {}
 
         self.executor = Executor(10, {
             Tag.LOAD: 1,
@@ -134,7 +62,6 @@ class Service:
             Tag.MSA : 5,
             Tag.DATA: 10 })
 
-        self.blob_id_map = BlobIdMap()
         self.storage = Storage()
         self.blobs = None
 
@@ -143,8 +70,6 @@ class Service:
         self.wiring = None
 
         self.rest_blob_id_map = RestBlobIdMap()
-
-        self.endpoint_factory = RouterTransactionFactory(self)
 
     def shutdown(self):
         logging.info("router_service shutdown()")
@@ -155,12 +80,6 @@ class Service:
             self.gc_thread.join()
 
         assert(self.executor.wait_empty(timeout=5))
-
-        with self.lock:
-            txx = [tx for id,tx in self.inflight.items()]
-            for tx in txx:
-                tx.abort()
-                tx.finalize()
 
         if self.wsgi_server:
             self.wsgi_server.shutdown()
@@ -220,26 +139,6 @@ class Service:
                                            flask_app)
             self.wsgi_server.serve_forever()
 
-    def get_router_transaction(self, host, storage_tx=None
-                               ) -> Optional[RouterTransaction]:
-        logging.info('get_router_transaction %s', host)
-        e = self.get_endpoint(host)
-        if e is None: return None
-        endpoint, tag, msa = e
-        tx = RouterTransaction(
-            self.executor, self.storage, self.blob_id_map, self.blobs,
-            endpoint, host, msa, tag, storage_tx)
-        if storage_tx is None:
-            tx.generate_rest_id(lambda: self.done(tx))
-        # xxx ttl/gc of these to be provided by rest service?
-        self._add_inflight(tx)
-
-        return tx
-
-    def done(self, tx):
-        logging.info('done %s', tx.rest_id)
-        self._del_inflight(tx)
-        tx.finalize()
 
     # -> Endpoint, Tag, is-msa
     def get_endpoint(self, host) -> Optional[Tuple[Any, int, bool]]:
@@ -247,12 +146,6 @@ class Service:
         if not endpoint: return None
         tag = Tag.MSA if msa else Tag.MX
         return endpoint, tag, msa
-
-    def handle(self, storage_tx):
-        transaction = self.get_router_transaction(storage_tx.host, storage_tx)
-        transaction.load()
-        self._del_inflight(transaction)
-        transaction.finalize()
 
     def handle_tx(self, storage_tx : TransactionCursor, endpoint : object):
         cursor_to_endpoint(storage_tx, endpoint)
@@ -282,16 +175,13 @@ class Service:
             return False
 
         logging.info("dequeued %d", storage_tx.id)
-        if self.rest_tx_factory:
-            # XXX there is a race that this can be selected
-            # between creation and writing the envelope
-            storage_tx.wait_attr_not_none('host')
-            endpoint, msa = self.wiring.get_endpoint(storage_tx.host)
-            tag = Tag.MSA if msa else Tag.MX
-            self.executor.enqueue(
-                tag, lambda: self.handle_tx(storage_tx, endpoint))
-        else:
-            self.executor.enqueue(tag, lambda: self.handle(storage_tx))
+        # XXX there is a race that this can be selected
+        # between creation and writing the envelope
+        storage_tx.wait_attr_not_none('host')
+        endpoint, msa = self.wiring.get_endpoint(storage_tx.host)
+        tag = Tag.MSA if msa else Tag.MX
+        self.executor.enqueue(
+            tag, lambda: self.handle_tx(storage_tx, endpoint))
         return True
 
     def dequeue(self):
@@ -310,38 +200,13 @@ class Service:
             # xxx wait for shutdown
             time.sleep(self.config.get_int('gc_interval'))
 
-    def _add_inflight(self, tx):
-        assert(tx.rest_id)
-        with self.lock:
-            assert(tx.rest_id not in self.inflight)
-            self.inflight[tx.rest_id] = tx
-
-    def _del_inflight(self, tx):
-        assert(tx.rest_id)
-        with self.lock:
-            del self.inflight[tx.rest_id]
-
     def _gc_inflight(self, idle_timeout=None):
         now = time.monotonic()
         logging.info('router_service _gc_inflight %d', idle_timeout)
 
-        if self.rest_tx_factory:
-            count = self.storage.gc_non_durable(idle_timeout)
-            logging.info('router_service _gc_inflight aborted %d', count)
-            return count
-
-        with self.lock:
-            dele = []
-            for rest_id, tx in self.inflight.items():
-                aborted = tx.abort_if_idle(idle_timeout, now)
-                if aborted:
-                    dele.append(tx)
-        for tx in dele:
-            # recovered (which are run sync in handle) aren't eligible
-            # for idle gc
-            # rest-initiated have done_cb = _del_inflight()
-            tx.finalize()
-            self._del_inflight(tx)
+        count = self.storage.gc_non_durable(idle_timeout)
+        logging.info('router_service _gc_inflight aborted %d', count)
+        return count
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,
