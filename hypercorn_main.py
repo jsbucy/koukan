97c7059a1b65e765da8434067e00fa536a18b3a2
John Bucy <bucy@gloop.org>
2024-04-26 15:52:43 -0700

    somehow never added hypercorn_main.py :facepalm:

---
+++ hypercorn_main.py
@@ -0,0 +1,18 @@
+from typing import List, Tuple
+import logging
+
+from asgiref.wsgi import WsgiToAsgi
+
+from hypercorn.config import Config
+import asyncio
+from hypercorn.asyncio import serve
+
+
+def run(bind : List[Tuple[str,int]], cert, key, app):
+    config = Config()
+    config.bind = [('%s:%d' % (h,p)) for h,p in bind]
+    if cert and key:
+        logging.debug('cert %s key %s', cert, key)
+        config.certfile = cert
+        config.keyfile = key
+    asyncio.run(serve(WsgiToAsgi(app), config))
