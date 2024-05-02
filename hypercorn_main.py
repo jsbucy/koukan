af4bbb70e5f400c9352b1e877dc136f2b56aa09d
John Bucy <bucy@gloop.org>
2024-05-02 10:20:28 -0700

    Try a2wsgi wsgi->asgi middleware instead of the asgiref one which seems to only run the app in one thread.

--- hypercorn_main.py
+++ hypercorn_main.py
@@ -1,7 +1,12 @@
 from typing import List, Tuple
 import logging
 
-from asgiref.wsgi import WsgiToAsgi
+# the asgiref implementation seems to run the app in a single
+# thread. Further down the sync_to_async stack it supports passing in
+# an executor but this isn't propagated all the way to this entry
+# point.
+#from asgiref.wsgi import WsgiToAsgi
+from a2wsgi import WSGIMiddleware
 
 from hypercorn.config import Config
 import asyncio
@@ -15,4 +20,4 @@ def run(bind : List[Tuple[str,int]], cert, key, app):
         logging.debug('cert %s key %s', cert, key)
         config.certfile = cert
         config.keyfile = key
-    asyncio.run(serve(WsgiToAsgi(app), config))
+    asyncio.run(serve(WSGIMiddleware(app), config))
