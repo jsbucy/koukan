acef05001ee0c367692b4c4b9cf2dc89f1d1e71b
John Bucy <bucy@gloop.org>
2024-12-05 15:30:23 -0800

    add type annotation

--- koukan/storage_schema.py
+++ koukan/storage_schema.py
@@ -1,7 +1,7 @@
 # Copyright The Koukan Authors
 # SPDX-License-Identifier: Apache-2.0
 from enum import IntEnum
-from typing import Dict
+from typing import Dict, Optional
 from koukan.rest_schema import BlobUri
 
 class InvalidActionException(Exception):
@@ -15,7 +15,7 @@ def internal_blob_prefix(b):
 
 TX_BODY='__internal_tx_body'
 
-def body_blob_uri(uri : BlobUri):
+def body_blob_uri(uri : BlobUri) -> Optional[BlobUri]:
     if uri.blob and internal_blob_prefix(uri.blob):
         raise ValueError()
     if not uri.tx_body:
