23f977d3f0832e6a7f79422993af91c5093e44cb
John Bucy <bucy@gloop.org>
2025-01-14 15:23:51 -0800

    refactor Exploder/Recipient

--- koukan/exploder.py
+++ koukan/exploder.py
@@ -7,7 +7,6 @@ import time
 from functools import partial, reduce
 
 from koukan.deadline import Deadline
-from koukan.executor import Executor
 from koukan.filter import (
     AsyncFilter,
     Mailbox,
@@ -28,21 +27,37 @@ from koukan.storage_schema import VersionConflictException
 
 class Recipient:
     filter : SyncFilter
-    tx :  TransactionMetadata
+    tx :  Optional[TransactionMetadata] = None
 
-    def __init__(self,
-                 filter : SyncFilter,
-                 tx :  TransactionMetadata):
+    def __init__(self, filter : SyncFilter):
         self.filter = filter
-        self.tx = tx
 
-    def on_update(self,
-                  delta : Optional[TransactionMetadata]) -> TransactionMetadata:
-        if delta is not None:
-            assert self.tx.merge_from(delta) is not None
-        else:
-            delta = self.tx.copy()
-        assert delta is not None
+    def first_update(self,
+                     tx : TransactionMetadata,
+                     output_chain : str, i : int):
+        self.tx = tx.copy()
+        self.tx.rest_id = None
+        self.tx.tx_db_id = None
+        self.tx.mail_response = None
+        self.tx.host = output_chain
+        self.tx.rcpt_to = [tx.rcpt_to[i]]
+        self.tx.rcpt_response = []
+        return self.filter.update(self.tx, self.tx.copy())
+
+    def update(self, delta : TransactionMetadata
+               ) -> Optional[TransactionMetadata]:
+        delta = delta.copy()
+        # XXX this field shouldn't be here?
+        if hasattr(delta, 'rcpt_to_list_offset'):
+            del delta.rcpt_to_list_offset
+        delta.rcpt_to = []
+        delta.rcpt_response = []
+        assert self.tx.merge_from(delta) is not None
+        # xxx drop body if prev rcpt/data perm err?
+        if not delta:
+            logging.debug('noop')
+            return TransactionMetadata()
+        logging.debug(self.tx)
         return self.filter.update(self.tx, delta)
 
 FilterFactory = Callable[[], Optional[SyncFilter]]
@@ -54,7 +69,6 @@ class Exploder(SyncFilter):
     rcpt_timeout : Optional[float] = None
     data_timeout : Optional[float] = None
     default_notification : Optional[dict] = None
-    executor : Executor
 
     rcpt_ok = False
     mail_from : Mailbox = None
@@ -64,7 +78,7 @@ class Exploder(SyncFilter):
     def __init__(self,
                  output_chain : str,
                  sync_factory : FilterFactory,
-                 executor : Executor,
+                 executor,
                  rcpt_timeout : Optional[float] = None,
                  data_timeout : Optional[float] = None,
                  default_notification : Optional[dict] = None):
@@ -74,23 +88,6 @@ class Exploder(SyncFilter):
         self.data_timeout = data_timeout
         self.recipients = []
         self.default_notification = default_notification
-        self.executor = executor
-
-    # run fns possibly in parallel
-    # def _run(self, fns : List[Optional[Callable]]):
-    #     assert not any([f for f in fns if f is None])
-    #     if len(fns) == 1:
-    #         fns[0]()
-    #         return True
-
-    #     futures = []
-    #     for fn in fns:
-    #         fut = self.executor.submit(fn)
-    #         assert fut is not None  # TODO better error handling
-    #         futures.append(fut)
-    #     # fns are something internal that respects timeouts
-    #     for fut in futures:
-    #         fut.result()
 
     def reduce_data_response(
             lhs : Union[None, Recipient],
@@ -109,41 +106,23 @@ class Exploder(SyncFilter):
         tx_orig = tx.copy()
         # TODO -> MailOkFilter
         if tx_delta.mail_from:
-            tx.mail_response = tx_delta.mail_response = Response(250, 'MAIL ok (exploder noop)')
+            tx.mail_response = tx_delta.mail_response = Response(
+                250, 'MAIL ok (exploder noop)')
 
         for i in range(0, len(tx.rcpt_to)):
             logging.debug(i)
-            downstream_delta = tx_delta.copy()
 
-            new_rcpt = None
             if i >= len(self.recipients):
                 logging.debug(i)
-                downstream_tx = tx_orig.copy()
-                if hasattr(downstream_tx, 'rcpt_to_list_offset'):
-                    del downstream_tx.rcpt_to_list_offset
-                downstream_tx.rest_id = None
-                downstream_tx.tx_db_id = None
-                downstream_tx.mail_response = None
-                downstream_tx.host = self.output_chain
-                downstream_tx.rcpt_to = [tx.rcpt_to[i]]
-                downstream_tx.rcpt_response = []
-                downstream_delta = None
-                new_rcpt = rcpt = Recipient(self.sync_factory(), downstream_tx)
+                rcpt = Recipient(self.sync_factory())
                 self.recipients.append(rcpt)
+                rcpt.first_update(tx, self.output_chain, i)
             else:
-                logging.debug(i)
-                # XXX this field shouldn't be here?
-                if hasattr(downstream_delta, 'rcpt_to_list_offset'):
-                    del downstream_delta.rcpt_to_list_offset
-                downstream_delta.rcpt_to = []
                 rcpt = self.recipients[i]
-                # xxx drop body if prev rcpt/data perm err?
-
-            if new_rcpt or downstream_delta:
-                rcpt.on_update(downstream_delta)
-                logging.debug(rcpt.tx)
-                # xxx need to propagate arbitrary fields from this
-                # upstream_delta to downstream_delta?
+                rcpt.update(tx_delta)
+            logging.debug(rcpt.tx)
+            # TODO need to propagate arbitrary fields from this
+            # upstream_delta to downstream_delta?
 
         assert len(self.recipients) == len(tx.rcpt_to)
 
@@ -154,7 +133,8 @@ class Exploder(SyncFilter):
             logging.debug('%s %s', rcpts, deadline.deadline_left())
             rcpt_next = []
             for rcpt in rcpts:
-                rcpt.filter.wait(rcpt.filter.version(), deadline.deadline_left())
+                rcpt.filter.wait(
+                    rcpt.filter.version(), deadline.deadline_left())
                 t = rcpt.filter.get()
                 assert rcpt.tx.delta(t) is not None  # check buggy filter
                 rcpt.tx = t
@@ -174,7 +154,8 @@ class Exploder(SyncFilter):
             return tx_orig.delta(tx)
 
         rcpt = reduce(Exploder.reduce_data_response,
-                      [ r for r in self.recipients if r.tx.rcpt_response[0].ok()])
+                      [ r for r in self.recipients
+                        if r.tx.rcpt_response[0].ok()])
         if rcpt is not None and rcpt.tx.data_response is not None:
             tx.data_response = Response(
                 rcpt.tx.data_response.code, 'exploder same response')
@@ -184,8 +165,9 @@ class Exploder(SyncFilter):
                 # XXX this will blackhole if unset!
                 notification=self.default_notification)
             for rcpt in self.recipients:
-                if rcpt.tx.rcpt_response[0].ok() and not rcpt.tx.data_response.ok():
-                    rcpt.on_update(retry_delta)
+                if (rcpt.tx.rcpt_response[0].ok() and
+                    not rcpt.tx.data_response.ok()):
+                    rcpt.update(retry_delta)
 
             tx.data_response = Response(250, 'exploder store and forward data')
 
