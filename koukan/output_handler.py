ca9f94286ae63becb23f8c60672e69e483e87fc8
John Bucy <bucy@gloop.org>
2025-05-12 15:36:43 -0700

    cleanups
    pytype passing
    drop old implementation
    carry forward not propagating upstream fields

--- koukan/output_handler.py
+++ koukan/output_handler.py
@@ -54,279 +54,12 @@ class OutputHandler:
         self.prev_tx = TransactionMetadata()
         self.tx = TransactionMetadata()
 
-    def _wait_downstream(self, delta : TransactionMetadata,
-                         ok_rcpt : bool
-                         ) -> Tuple[Optional[bool], Optional[Response]]:
-        # xxx req_inflight()?
-        # xxx delta vs cursor.tx?
-        body = self.cursor.tx.body
-        assert isinstance(body, Union[None, Blob, MessageBuilderSpec])
-        if ((delta.mail_from is not None) or
-            bool(delta.rcpt_to) or
-            ((body is not None) and body.finalized())):
-            return False, None
-
-        timeout = self.env_timeout
-        msg = 'envelope'
-        if ok_rcpt:
-            timeout = self.data_timeout
-            msg = 'body'
-
-        if not self.cursor.wait(timeout):
-            logging.debug(
-                'OutputHandler._output() %s timeout %s=%d',
-                self.rest_id, msg, timeout)
-            return None, Response(
-                400, 'OutputHandler downstream timeout (%s=%d)' % (
-                    msg, timeout))
-        self.cursor.load()
-        return True, None
-
-    def _output(self) -> Optional[Response]:
-        ok_rcpt = False
-        # full vector after last upstream update
-        last_tx = None
-        upstream_tx = None
-
-        # TODO clean up control flow
-        # while True:
-        #   # first call or small chance write_envelope will return new reqs
-        #   if not cursor.tx.req_inflight():
-        #     prev = cursor.tx.copy()
-        #     wait_downstream()
-        #     delta = prev.delta(cursor.tx)
-        #     precondition check: e.g. delta.body after all rcpts failed
-        #       delta = cancel -> upstream
-        #       return precondition error downstream
-
-        #   upstream_delta = output_chain.on_update(tx, delta)
-        #   done = mail_resp.err or data_response or req_inflight()  # bug
-        #   if done:
-        #     final attempt reason/notification logic
-        #   write_envelope(upstream_delta)
-        #   if done:
-        #     break
-
-        while True:
-            logging.debug('OutputHandler._output() %s db tx %s input done %s',
-                          self.rest_id, self.cursor.tx, self.cursor.input_done)
-
-            cursor_tx = self.cursor.tx.copy()
-            # NOTE: possible to read finalized body but input_done == False
-            if last_tx is None:
-                last_tx = cursor_tx.copy()
-                upstream_tx = cursor_tx.copy()
-                delta = last_tx.copy()
-            else:
-                delta = last_tx.delta(cursor_tx)
-                last_tx = cursor_tx.copy()
-                assert delta is not None
-                assert upstream_tx.merge_from(delta) is not None
-
-            assert len(last_tx.rcpt_to) >= len(last_tx.rcpt_response)
-            assert len(upstream_tx.rcpt_to) >= len(upstream_tx.rcpt_response)
-
-            # XXX drop some fields from upstream_tx:
-            # notification, retry
-            # these have REST_CREATE/... validity but we are handling it here
-            for field in ['notification', 'retry', 'final_attempt_reason']:
-                for obj in [last_tx, upstream_tx, delta]:
-                    if getattr(obj, field) is not None:
-                        delattr(obj, field)
-
-            # have_body here is really a proxy for "envelope done"
-            have_body = self.cursor.tx.body is not None
-            if ((not delta.rcpt_to) and (not ok_rcpt) and have_body) or (
-                    delta.cancelled):
-                delta = TransactionMetadata(cancelled=True)
-                assert upstream_tx.merge_from(delta) is not None
-                self.endpoint.on_update(upstream_tx, delta)
-
-                # all recipients so far have failed and we aren't
-                # waiting for any more (have_body) -> we're done
-                if len(self.cursor.tx.rcpt_to) == 1:
-                    return self.cursor.tx.rcpt_response[0]
-                else:
-                    # placeholder response, this is only used for
-                    # notification logic for
-                    # post-exploder/single-recipient tx
-                    return Response(400, 'OutputHandler all recipients failed')
-
-            waited, err = self._wait_downstream(delta, ok_rcpt)
-            if err is not None:
-                return err
-            elif waited:
-                continue
-
-            logging.info('OutputHandler._output() %s body %s',
-                         self.rest_id, upstream_tx.body)
-
-            # no new reqs in delta can happen e.g. if blob upload
-            # ping'd last_update
-            if delta.mail_from is None and not delta.rcpt_to and not delta.body:
-                logging.info('OutputHandler._output() %s no reqs', self.rest_id)
-                continue
-
-            upstream_delta = self.endpoint.on_update(upstream_tx, delta)
-            logging.info('OutputHandler._output() %s '
-                         'tx after upstream update %s',
-                         self.rest_id, upstream_tx)
-
-            if upstream_delta is None or upstream_tx.req_inflight():
-                logging.warning('OutputHandler._output() %s '
-                                'BUG empty upstream_delta %s',
-                                self.rest_id, upstream_delta)
-                return None  # internal error
-            assert len(upstream_tx.rcpt_response) <= len(upstream_tx.rcpt_to)
-
-            # if we're short-circuiting to ourselves, these fields will clash
-            for field in ['final_attempt_reason', 'attempt_count']:
-                for t in [upstream_tx, upstream_delta]:
-                    if getattr(t, field) is not None:
-                        delattr(t, field)
-
-            for i in range(0,5):
-                try:
-                    self.cursor.write_envelope(upstream_delta)
-                    assert last_tx.merge_from(upstream_delta) is not None
-                    break
-                except VersionConflictException:
-                    logging.info(
-                        'OutputHandler._output() VersionConflictException %s ',
-                        self.rest_id)
-                    if i == 4:
-                        raise
-                    backoff(i)
-                    self.cursor.load()
-
-            if (upstream_delta.mail_response is not None and
-                upstream_delta.mail_response.err()):
-                return upstream_delta.mail_response
-            if not ok_rcpt and any(
-                    [r.ok() for r in upstream_delta.rcpt_response]):
-                ok_rcpt = True
-            if upstream_delta.data_response is not None:
-                return upstream_delta.data_response
-
-    def handle_old(self):
-        logging.info('OutputHandler.handle() %s', self.rest_id)
-
-        try:
-            final_attempt_reason = None
-            # _cursor_to_endpoint() could be terminated by an uncaught
-            # exception due to a bug. We don't know whether this is
-            # due to something in the tx deterministically tickling a
-            # bug in the code or due to a bug handling a transient
-            # failure (e.g. the executor overflow thing w/exploder
-            # which we handled in 2eb7a4f0 but there are undoubtedly
-            # others) that would succeed if you retry. To try to split
-            # the difference on this, preload next_attempt_time with
-            # 1h so that transient errors will eventually get retried
-            # but we won't crashloop excessively on a bad tx. This is
-            # a last resort against bugs.
-            next_attempt_time = time.time() + self.retry_params.get(
-                'bug_retry', 3600)
-            delta = TransactionMetadata()
-            notification_done = False
-
-            if not self.cursor.no_final_notification:
-                final_attempt_reason, next_attempt_time = (
-                    self._cursor_to_endpoint())
-                # xxx why is this here?
-                delta.attempt_count = self.cursor.attempt_id
-            else:
-                # post facto notification
-                # leave the existing value
-                final_attempt_reason = None
-
-            if self.cursor.tx.notification:
-                self._maybe_send_notification(final_attempt_reason)
-                notification_done = True
-        except Exception as e:
-            logging.exception('OutputHandler.handle(): _cursor_to_endpoint()')
-        finally:
-            for i in range(0,5):
-                try:
-                    # TODO it probably wouldn't be hard to merge this
-                    # write with the one at the end of _output()
-
-                    # postcondition of _cursor_to_endpoint() is that all
-                    # reqs have responses. Populate with a temp err here
-                    # in case of a bug, otherwise the downstream will hang
-                    # up to some timeout waiting for a response that will
-                    # never come.
-                    err = TransactionMetadata()
-                    err_resp = Response(
-                        450, 'internal error: OutputHandler failed to populate response')
-                    self.cursor.tx.fill_inflight_responses(err_resp, err)
-
-                    if (self.cursor.tx.body and self.cursor.input_done and
-                        self.cursor.tx.data_response is None):
-                        self.cursor.tx.data_response = err_resp
-                    if err:
-                        logging.warning('missing responses %s', self.cursor.tx)
-                        delta.merge_from(err)
-                    self.cursor.write_envelope(
-                        delta,
-                        final_attempt_reason=final_attempt_reason,
-                        next_attempt_time=next_attempt_time,
-                        finalize_attempt=True,
-                        # True clears no_final_notification
-                        notification_done=notification_done)
-                    break
-                except VersionConflictException:
-                    if i == 4:
-                        raise
-                    backoff(i)
-                    self.cursor.load()
-
-    def _cursor_to_endpoint(self) -> Tuple[Optional[str], Optional[int]]:
-        logging.debug('OutputHandler._cursor_to_endpoint() %s', self.rest_id)
-
-        resp = self._output()
-        if resp is None:
-            logging.warning('OutputHandler._cursor_to_endpoint() %s abort',
-                            self.rest_id)
-            resp = Response(400, 'output handler abort')
-        logging.info('OutputHandler._cursor_to_endpoint() %s done %s',
-                     self.rest_id, resp)
-
-        next_attempt_time = None
-        final_attempt_reason = None
-        if self.cursor.tx.cancelled:
-            pass  # leave existing value
-        elif resp.ok():
-            final_attempt_reason = 'upstream response success'
-        elif resp.perm():
-            final_attempt_reason = 'upstream response permfail'
-        elif (not self.cursor.input_done) and (self.cursor.tx.retry is None):
-            # if we early-returned due to upstream temp err before
-            # receiving all of the body, don't set
-            # final_attempt_reason if retries are enabled so it is
-            # recoverable at such time as we get the rest of the
-            # body/input_done
-            final_attempt_reason = 'downstream timeout'
-        else:
-            # assert len(self.cursor.tx.rcpt_to) == 1
-            # assert self.cursor.tx.body.finalized()
-            final_attempt_reason, next_attempt_time = (
-                self._next_attempt_time(time.time()))
-
-        logging.debug(
-            'OutputHandler._cursor_to_endpoint() %s attempt %d retry %s '
-            'final_attempt_reason %s',
-            self.rest_id, self.cursor.attempt_id, self.cursor.tx.retry,
-            final_attempt_reason)
-
-        return final_attempt_reason, next_attempt_time
-
     # one iteration of output
     # - wait for new downstream if necessary
     # - emit to output chain
     # - completion/next attempt/notification logic
     # -> delta, kwargs for write_envelope()
     def _handle_once(self) -> Tuple[TransactionMetadata, dict]:
-        upstream_delta = None
         # cursor.no_final_notification: tx.notification was None when
         # final_attempt_reason was written iow notifications were enabled after
         # the tx already failed (Exploder)
@@ -335,16 +68,21 @@ class OutputHandler:
         # we may have picked up new downstream from the previous
         # write_envelope()
         downstream_timeout = False
-        logging.debug(self.tx)
         if (not self.cursor.no_final_notification and
             not self.cursor.input_done and
             not self.prev_tx.delta(self.tx) and  # no new downstream
             not self.tx.cancelled):
+            # TODO until we get chunked uploads merged in aiosmtpd,
+            # probably want gateway/SmtpHandler to keepalive/ping
+            # during data upload so this can time out quickly if the
+            # gw crashed
             if not self.cursor.wait(self.env_timeout):  # xxx
                 logging.debug('downstream timeout')
                 downstream_timeout = True
             else:
-                self.tx = self.cursor.load().copy()
+                tx = self.cursor.load()
+                assert tx is not None
+                self.tx = tx.copy()
         # drop some fields from the tx that's going upstream
         # OH consumes these fields but they should not propagate to the
         # output chain/upstream rest/gateway etc
@@ -363,7 +101,15 @@ class OutputHandler:
             self.tx.merge_from(upstream_delta)
         elif self.tx.req_inflight() or self.tx.cancelled:
             upstream_delta = self.endpoint.on_update(self.tx, delta)
-            logging.debug(self.cursor.tx)
+            assert upstream_delta is not None
+
+            # similarly to above, if rest output to router
+            # (short-circuit: submission -> egress), these fields will clash
+            for field in ['final_attempt_reason', 'attempt_count']:
+                for t in [self.tx, upstream_delta]:
+                    if getattr(t, field) is not None:
+                        delattr(t, field)
+
             self.prev_tx = self.tx.copy()
             # postcondition check: !req_inflight() in handle() finally:
         else:
@@ -416,6 +162,7 @@ class OutputHandler:
 
     def handle(self):
         done = False
+        logging.debug('OutputHandler.handle %s', self.cursor.rest_id)
         while not done:
             try:
                 # pre-load results as a last resort against bugs:
@@ -446,7 +193,6 @@ class OutputHandler:
                 self.tx.fill_inflight_responses(err_resp, delta)
                 for i in range(0,5):
                     try:
-                        logging.debug(self.cursor.tx)
                         self.cursor.write_envelope(delta, **env_kwargs)
                         break
                     except VersionConflictException:
