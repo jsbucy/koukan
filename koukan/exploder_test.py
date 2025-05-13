jsbucy/koukan

commit cd6c8bd2f7dd6533718d011dcf1cc75be72cec30

body/message_builder output workflow improvements
1: bug: storage skip _maybe_write_blob() if body not in delta, was
spuriously setting input_done
2: latent bug/quirk: storage _load_blobs() ORDER BY id
  the new body delta stuff in this patch requires this
3: OutputHandler do not clear body if not finalized
4: TransactionMetadata.delta() emit body if it grew but detect/fail if
it changed in some other unexpected way
5: message_builder_filter noop if message builder spec not finalized

The net of this is to give message_builder_filter a chance to reject
the json on the first update call before we possibly hang on the
upstream so it can report downstream to the client ~immediately.
TODO: implement this.

koukan/exploder.py
@@ -211,11 +211,8 @@ class Exploder(SyncFilter):
                 rcpt.tx.data_response.code,
                 rcpt.tx.data_response.message + ' (Exploder same response)')
         elif body.finalized():
-            # OutputHandler currently only sends finalized body so we
-            # always take this branch today.
-            # Moreover, Storage currently does not allow reusing
-            # !finalized blob so even if OH streamed the body out, we
-            # would buffer here it until finalized.
+            # Storage currently does not allow reusing !finalized blob
+            # so we must buffer here it until finalized.
             retry_delta = TransactionMetadata(
                 retry = {},
                 # XXX this will blackhole if unset!

koukan/blob.py
@@ -30,6 +30,10 @@ class Blob(ABC):
         cl = self.content_length()
         return cl is not None and cl == self.len()
 
+    # returns True if rhs is a proper superset of self,
+    # False if they are the same, else None
+    def delta(self, rhs) -> Optional[bool]:
+        raise NotImplementedError()
 
 class WritableBlob(ABC):
     @abstractmethod
@@ -92,6 +96,13 @@ class InlineBlob(Blob, WritableBlob):
         self.d = d
         self._content_length = len(d) if last else content_length
         self._rest_id = rest_id
+
+    def delta(self, rhs) -> Optional[bool]:
+        if not isinstance(rhs, InlineBlob):
+            return None
+        if not rhs.d.startswith(self.d):
+            return None
+        return rhs.d != self.d
 
     def len(self):
         return len(self.d)

koukan/filter.py
@@ -603,6 +603,14 @@ class TransactionMetadata:
             if isinstance(old_v, list) != isinstance(new_v, list):
                 logging.debug('list-ness mismatch')
                 return None  # invalid
+            if f == 'body' and old_v is not None and new_v is not None:
+                body_delta = old_v.delta(new_v)
+                if body_delta is None:
+                    return None
+                if body_delta:
+                    setattr(out, f, new_v)
+                continue
+
             if not isinstance(old_v, list):
                 # use the old value, assume the new one is the same
                 # TODO could verify that old_v == new_v
@@ -649,6 +657,15 @@ class TransactionMetadata:
                return None  # invalid
             if (old_v is None) and (new_v is not None):
                 setattr(out, f, new_v)
+                continue
+
+            # emit body in the delta if it changed
+            if f == 'body' and old_v is not None and new_v is not None:
+                body_delta = old_v.delta(new_v)
+                if body_delta is None:
+                    return None
+                if body_delta:
+                    setattr(out, f, new_v)
                 continue
 
             # XXX TxField.is_list?

koukan/message_builder.py
@@ -85,6 +85,26 @@ class MessageBuilderSpec:
         return (len(self.ids) == len(self.blobs) and
                 not any([not b.finalized() for b in self.blobs]))
 
+    def delta(self, rhs) -> Optional[bool]:
+        if not isinstance(rhs, MessageBuilderSpec):
+            return None
+        if self.json != rhs.json:
+            return None
+        if len(self.blobs) != len(rhs.blobs):
+            logging.debug('blobs len')
+            return None
+        out = False
+        for i,blob in enumerate(self.blobs):
+            blob_delta = blob.delta(rhs.blobs[i])
+            if blob_delta is None:
+                logging.debug(i)
+                logging.debug(self.blobs)
+                logging.debug(rhs.blobs)
+                return None
+            out |= blob_delta
+        return out
+
+
     def __repr__(self):
         return '%s %s' % (self.json, self.blobs)
 

koukan/message_builder_filter.py
@@ -24,50 +24,56 @@ class MessageBuilderFilter(SyncFilter):
                   tx_delta : TransactionMetadata
                   ) -> Optional[TransactionMetadata] :
         logging.debug(tx.body)
-        if ((tx.body is None) or (not isinstance(tx.body, MessageBuilderSpec))) and (self.body is None):
+        body = tx.body
+        if not isinstance(body, MessageBuilderSpec):
             return self.upstream.on_update(tx, tx_delta)
+        if not body.finalized():
+            # TODO fastfail on invalid tx.body.json here
+            downstream_tx = tx.copy()
+            downstream_delta = tx_delta.copy()
+            downstream_tx.body = downstream_delta.body = None
+            upstream_delta = self.upstream.on_update(
+                downstream_tx, downstream_delta)
+            tx.merge_from(upstream_delta)
+            return upstream_delta
 
-        built = False
-        if tx_delta.body is not None and isinstance(
-                tx_delta.body, MessageBuilderSpec):
-            builder = MessageBuilder(
-                tx_delta.body.json,
-                # xxx always BlobCursor?
-                { blob.rest_id(): blob for blob in tx_delta.body.blobs })
 
-            try:
-                file = TemporaryFile('w+b')
-                assert isinstance(file, IOBase)
-                builder.build(file)
-                file.flush()
-                self.body = FileLikeBlob(file, finalized=True)
-            except:
-                # last-ditch handling in case the json tickled a bug
+        builder = MessageBuilder(
+            tx_delta.body.json,
+            # xxx always BlobCursor?
+            { blob.rest_id(): blob for blob in tx_delta.body.blobs })
 
-                # TODO I'm not confident the client will figure out
-                # what the actual problem is from these error
-                # responses. We need to do some validation of the
-                # message builder spec closer to RestHandler so that
-                # it can return an http 400 synchronously to the
-                # original tx creation POST.
-                logging.exception('unexpected exception in MessageBuilder')
-                err = TransactionMetadata()
-                msg = ('unexpected exception in MessageBuilder, '
-                       'likely invalid message_builder json')
-                tx.fill_inflight_responses(Response(450, msg), err)
-                err.data_response = Response(550, msg)
-                tx.merge_from(err)
-                return err
+        try:
+            file = TemporaryFile('w+b')
+            assert isinstance(file, IOBase)
+            builder.build(file)
+            file.flush()
+            self.body = FileLikeBlob(file, finalized=True)
+        except:
+            # last-ditch handling in case the json tickled a bug
 
-            logging.debug('MessageBuilderFilter.on_update %d %s',
-                          self.body.len(), self.body.content_length())
-            built = True
+            # TODO I'm not confident the client will figure out
+            # what the actual problem is from these error
+            # responses. We need to do some validation of the
+            # message builder spec closer to RestHandler so that
+            # it can return an http 400 synchronously to the
+            # original tx creation POST.
+            logging.exception('unexpected exception in MessageBuilder')
+            err = TransactionMetadata()
+            msg = ('unexpected exception in MessageBuilder, '
+                   'likely invalid message_builder json')
+            tx.fill_inflight_responses(Response(450, msg), err)
+            err.data_response = Response(550, msg)
+            tx.merge_from(err)
+            return err
+
+        logging.debug('MessageBuilderFilter.on_update %d %s',
+                      self.body.len(), self.body.content_length())
 
         downstream_tx = tx.copy()
         downstream_delta = tx_delta.copy()
 
-        downstream_tx.body = self.body
-        downstream_delta.body = self.body if built else None
+        downstream_tx.body = downstream_delta.body = self.body
 
         upstream_delta = self.upstream.on_update(
             downstream_tx, downstream_delta)

koukan/message_builder_filter_test.py
@@ -37,6 +37,7 @@ class MessageBuilderFilterTest(unittest.TestCase):
             blobs = [InlineBlob(b'hello, world!', last=True,
                                 rest_id='blob_rest_id')]
         )
+        tx.body.check_ids()
 
         def exp(tx, delta):
             self.assertTrue(isinstance(delta.body, Blob))

koukan/output_handler.py
@@ -54,9 +54,12 @@ class OutputHandler:
                          ok_rcpt : bool
                          ) -> Tuple[Optional[bool], Optional[Response]]:
         # xxx req_inflight()?
+        # xxx delta vs cursor.tx?
+        body = self.cursor.tx.body
+        assert isinstance(body, Union[None, Blob, MessageBuilderSpec])
         if ((delta.mail_from is not None) or
             bool(delta.rcpt_to) or
-            (self.cursor.tx.body and self.cursor.tx.body.finalized())):
+            ((body is not None) and body.finalized())):
             return False, None
 
         timeout = self.env_timeout
@@ -113,8 +116,6 @@ class OutputHandler:
                     body = b
                 else:
                     raise ValueError()
-                if not body.finalized():
-                    cursor_tx.body = None
             if last_tx is None:
                 last_tx = cursor_tx.copy()
                 upstream_tx = cursor_tx.copy()
@@ -144,10 +145,6 @@ class OutputHandler:
                 assert upstream_tx.merge_from(delta) is not None
                 self.endpoint.on_update(upstream_tx, delta)
 
-                # XXX because of the way we clear non-finalized body
-                # (above), won't get data_response precondition err if
-                # all rcpts failed, need to set here?
-
                 # all recipients so far have failed and we aren't
                 # waiting for any more (have_body) -> we're done
                 if len(self.cursor.tx.rcpt_to) == 1:
@@ -164,7 +161,7 @@ class OutputHandler:
             elif waited:
                 continue
 
-            logging.info('OutputHandler._output() %s body blob %s',
+            logging.info('OutputHandler._output() %s body %s',
                          self.rest_id, upstream_tx.body)
 
             # no new reqs in delta can happen e.g. if blob upload
@@ -177,7 +174,8 @@ class OutputHandler:
             logging.info('OutputHandler._output() %s '
                          'tx after upstream update %s',
                          self.rest_id, upstream_tx)
-            if upstream_delta is None or not(upstream_delta):
+
+            if upstream_delta is None or upstream_tx.req_inflight():
                 logging.warning('OutputHandler._output() %s '
                                 'BUG empty upstream_delta %s',
                                 self.rest_id, upstream_delta)
@@ -417,13 +415,14 @@ class OutputHandler:
             return
 
         orig_headers : str
-        if self.cursor.tx.body:
-            if isinstance(self.cursor.tx.body, MessageBuilderSpec):
-                builder = MessageBuilder(self.cursor.tx.body.json,
-                                         blobs={})  # blobs not needed here
+        body = self.cursor.tx.body
+        if body:
+            if isinstance(body, MessageBuilderSpec):
+                # blobs not needed here
+                builder = MessageBuilder(body.json, blobs={})
                 orig_headers = builder.build_headers_for_notification().decode(
                     'utf-8')
-            elif isinstance(self.cursor.tx.body, Blob):
+            elif isinstance(body, Blob):
                 h = read_headers(self.cursor.tx.body)
                 orig_headers = h if h is not None else ''
             else:

koukan/router_service_test.py
@@ -1029,16 +1029,18 @@ class RouterServiceTest(unittest.TestCase):
 
         def exp_body(tx, tx_delta):
             logging.debug('test_message_builder.exp_body %s', tx)
+            if tx.body is None:
+                return TransactionMetadata()
             body = tx.body.pread(0)
             logging.debug(body)
             self.assertIn(b'subject: hello\r\n', body)
             #self.assertIn(b, body)  # base64
-            updated_tx = tx.copy()
-            updated_tx.data_response = Response(203)
-            upstream_delta = tx.delta(updated_tx)
+            upstream_delta = TransactionMetadata(
+                data_response = Response(203))
             self.assertIsNotNone(tx.merge_from(upstream_delta))
             return upstream_delta
 
+        upstream_endpoint.add_expectation(exp_body)
         upstream_endpoint.add_expectation(exp_body)
 
         rest_endpoint.on_update(tx, tx.copy(), 1)

commit 37c146a2b1269a7426b4aea827f08dc2da2bee4d

Exploder buffer !finalized body (regression from cd6c8bd2)
otherwise this can eventually get to storage (TODO better
precondition checks there) and try to create a blobref multiple times
-> db exception

possibly this made e.g. router_service_test flaky and I didn't notice?
It seems like it's sensitive to timing whether the test tickles it...

koukan/BUILD
@@ -323,6 +323,7 @@ py_test(name='exploder_test',
         deps=[':exploder',
               ':async_filter_wrapper',
               ':blob',
+              ':fake_endpoints',
               ':filter',
               ':postgres_test_utils',
               ':response',

koukan/exploder.py
@@ -140,11 +140,19 @@ class Exploder(SyncFilter):
         return lhs
 
     def on_update(self,
-                  tx : TransactionMetadata,
-                  tx_delta : TransactionMetadata
+                  downstream_tx : TransactionMetadata,
+                  downstream_delta : TransactionMetadata
                   ) -> Optional[TransactionMetadata]:
-        tx_orig = tx.copy()
-        tx_delta = tx_delta.copy()
+        tx_orig = downstream_tx.copy()
+        tx = downstream_tx.copy()
+        tx_delta = downstream_delta.copy()
+
+        # OutputHandler may send but Storage currently does not accept
+        # reusing !finalized blob so we must buffer incomplete body here.
+        if tx.body is not None and not tx.body.finalized():
+            tx_orig.body = tx.body = tx_delta.body = None
+            if not tx_delta:
+                return TransactionMetadata()
 
         if tx_delta.mail_from:
             tx.mail_response = tx_delta.mail_response = Response(
@@ -192,7 +200,9 @@ class Exploder(SyncFilter):
                 tx.data_response = rcpt.tx.data_response
 
         if (body is None) or (tx.data_response is not None):
-            return tx_orig.delta(tx)
+            upstream_delta = tx_orig.delta(tx)
+            downstream_tx.merge_from(upstream_delta)
+            return upstream_delta
 
         # If all rcpts with rcpt_response.ok() have the same
         # data_response.major_code(), return that.  The most likely
@@ -210,9 +220,7 @@ class Exploder(SyncFilter):
             tx.data_response = Response(
                 rcpt.tx.data_response.code,
                 rcpt.tx.data_response.message + ' (Exploder same response)')
-        elif body.finalized():
-            # Storage currently does not allow reusing !finalized blob
-            # so we must buffer here it until finalized.
+        else:
             retry_delta = TransactionMetadata(
                 retry = {},
                 # XXX this will blackhole if unset!
@@ -225,4 +233,6 @@ class Exploder(SyncFilter):
 
             tx.data_response = Response(250, 'DATA ok (Exploder store&forward)')
 
-        return tx_orig.delta(tx)
+        upstream_delta = tx_orig.delta(tx)
+        downstream_tx.merge_from(upstream_delta)
+        return upstream_delta

koukan/exploder_test.py
@@ -7,6 +7,7 @@ import logging
 import time
 from functools import partial
 from threading import Thread
+import traceback
 
 from koukan.deadline import Deadline
 from koukan.storage import Storage, TransactionCursor
@@ -24,8 +25,8 @@ import koukan.postgres_test_utils as postgres_test_utils
 from koukan.async_filter_wrapper import AsyncFilterWrapper
 
 from koukan.rest_schema import BlobUri
+from koukan.fake_endpoints import MockAsyncFilter
 
-import traceback
 
 def setUpModule():
     postgres_test_utils.setUpModule()
@@ -527,6 +528,57 @@ class ExploderTest(unittest.TestCase):
         self.assertEqual(250, tx.mail_response.code)
         self.assertEqual(1, len(tx.rcpt_response))
         self.assertEqual(451, tx.rcpt_response[0].code)
+
+    def test_partial_body(self):
+        upstream = MockAsyncFilter()
+        exploder = Exploder('output-chain',
+                            lambda: upstream,
+                            rcpt_timeout=5,
+                            default_notification={})
+
+        def exp_update(tx, delta):
+            self.assertIsNone(tx.body)
+            upstream_delta = TransactionMetadata(
+                mail_response=Response(201),
+                rcpt_response=[Response(202)])
+            tx.merge_from(upstream_delta)
+            return upstream_delta
+
+        upstream.expect_update(exp_update)
+
+        body = b'hello'
+
+        tx = TransactionMetadata(
+            mail_from=Mailbox('alice'),
+            rcpt_to=[Mailbox('bob')],
+            body=InlineBlob(body))
+        upstream_delta = exploder.on_update(tx, tx.copy())
+        self.assertEqual(250, tx.mail_response.code)
+        self.assertEqual([202], [r.code for r in tx.rcpt_response])
+        self.assertIsNone(tx.data_response)
+
+        # empty delta after we drop non-finalized body -> early
+        # return, no upstream update
+        tx_delta = TransactionMetadata()
+        body += b', world'
+        tx.body = tx_delta.body = InlineBlob(body)
+        upstream_delta = exploder.on_update(tx, tx_delta)
+        self.assertFalse(upstream_delta)
+
+        def exp_body(tx, delta):
+            self.assertTrue(tx.body.finalized())
+            upstream_delta = TransactionMetadata(
+                data_response=Response(203))
+            tx.merge_from(upstream_delta)
+            return upstream_delta
+        upstream.expect_update(exp_body)
+
+        body += b'!'
+        tx.body = tx_delta.body = InlineBlob(body, last=True)
+        upstream_delta = exploder.on_update(tx, tx_delta)
+        self.assertEqual(250, tx.mail_response.code)
+        self.assertEqual([202], [r.code for r in tx.rcpt_response])
+        self.assertEqual(203, tx.data_response.code)
 
 
 if __name__ == '__main__':