b3cfdcd7c948417397e978b55f2c27f0c1544537
John Bucy <bucy@gloop.org>
2025-08-16 12:37:50 -0700

    WIP start refactoring RecipientRouterFilter to handle multiple recipients
    ... so it can be chained downstream of exploder, as alluded to in
    41a63fb4. It becomes CoroutineProxyFilter for now. In principle it
    seems like the upstream_rcpt mapping/merging could be handled by
    FilterResult.
    
    TODO
    The upstream dest fields: rest_endpoint, remote_host, options become
    per-rcpt, probably just shove in Mailbox.
    
    TBD
    - who populates mail_response if (the only) rcpt fails here
    - whether to drop body upstream if no valid rcpt

--- koukan/recipient_router_filter.py
+++ koukan/recipient_router_filter.py
@@ -1,6 +1,13 @@
 # Copyright The Koukan Authors
 # SPDX-License-Identifier: Apache-2.0
-from typing import Any, Callable, Dict, List, Optional, Tuple, TypeAlias
+from typing import (
+    Awaitable,
+    Any,
+    Callable,
+    Dict,
+    List,
+    Optional,
+    Tuple )
 from abc import ABC, abstractmethod
 import logging
 
@@ -10,7 +17,7 @@ from koukan.filter import (
     Mailbox,
     Resolution,
     TransactionMetadata )
-from koukan.filter_chain import FilterResult, Filter
+from koukan.filter_chain import CoroutineProxyFilter, FilterResult
 
 class Destination:
     rest_endpoint : Optional[str] = None
@@ -43,48 +50,70 @@ class RoutingPolicy(ABC):
         raise NotImplementedError
 
 
-class RecipientRouterFilter(Filter):
+class RecipientRouterFilter(CoroutineProxyFilter):
     policy : RoutingPolicy
+    # upstream_tx.rcpt_to[i] == downstream_tx[upstream_rcpt[i]]
+    upstream_rcpt : List[int]
 
     def __init__(self, policy : RoutingPolicy):
         self.policy = policy
+        self.upstream_rcpt = []
 
-    def _route(self):
+    def _route(self, mailbox) -> Optional[Response]:
         tx = self.downstream_tx
         logging.debug('RecipientRouterFilter._route() %s', tx)
-        mailbox = tx.rcpt_to[0]
         assert mailbox is not None
         dest, resp = self.policy.endpoint_for_rcpt(mailbox.mailbox)
+        logging.debug(dest)
+        logging.debug(resp)
 
-        # TODO Sending responses upstream the way this does
-        # immediately trips an assertion in FilterChain. This should
-        # be ProxyFilter: clear the bad rcpt from the upstream tx and
-        # set the error in the downstream path and let FilterChain
-        # decide what to do with the remainder of the delta.
-        # TODO if we ever have multi-rcpt in the output chain, this
-        # should validate that other mailboxes route to the same place
         if resp and resp.err():
-            if tx.mail_from and tx.mail_response is None:
-                tx.mail_response = Response(
-                    250, 'MAIL ok (RecipientRouterFilter)')
-            tx.rcpt_response = [resp]
-            return
+            # xxx this can potentially keep going?
+            # if tx.mail_from and tx.mail_response is None:
+            #     tx.mail_response = Response(
+            #         250, 'MAIL ok (RecipientRouterFilter)')
+            return resp
         elif dest is None:
-            return
+            return None
 
-        tx.rest_endpoint = dest.rest_endpoint
+        # XXX err if any of this doesn't match previous.  Eventually
+        # this becomes per-rcpt and the terminal/sink filter can sort
+        # that out.
+        self.upstream_tx.rest_endpoint = dest.rest_endpoint
         if dest.remote_host is not None:
-            tx.resolution = Resolution(dest.remote_host)
+            self.upstream_tx.resolution = Resolution(dest.remote_host)
         if dest.http_host is not None:
-            tx.upstream_http_host = dest.http_host
-        tx.options = dest.options
+            self.upstream_tx.upstream_http_host = dest.http_host
+        self.upstream_tx.options = dest.options
+        return None
 
-    def on_update(self, tx_delta : TransactionMetadata) -> FilterResult:
-        if (tx_delta.rcpt_to and
+    async def on_update(
+            self, tx_delta : TransactionMetadata,
+            upstream : Callable[[], Awaitable[TransactionMetadata]]):
+        rcpt_to = tx_delta.rcpt_to
+        tx_delta.rcpt_to = []
+
+        # xxx drop body if no good rcpt?
+
+        for i,rcpt in enumerate(rcpt_to):
+            off = i + tx_delta.rcpt_to_list_offset
             # this may be chained multiple times; noop if a previous
             # instance already routed
-            self.downstream_tx.rest_endpoint is None and
-            self.downstream_tx.options is None):
-            self._route()
+            resp = None
+            if (self.downstream_tx.rest_endpoint is None and
+                self.downstream_tx.options is None):
+                resp = self._route(rcpt)
+            assert resp is None or resp.err()
+            self.downstream_tx.rcpt_response.append(resp)
+            if resp is None:
+                tx_delta.rcpt_to.append(rcpt)
+                self.upstream_rcpt.append(i)
 
-        return FilterResult()
+        self.upstream_tx.merge_from(tx_delta)
+        upstream_delta = await upstream()
+        upstream_rcpt_response = upstream_delta.rcpt_response
+        upstream_delta.rcpt_response = None
+        self.downstream_tx.merge_from(upstream_delta)
+        logging.debug(self.upstream_tx)
+        for i,resp in enumerate(upstream_rcpt_response):
+            self.downstream_tx.rcpt_response[self.upstream_rcpt[i]] = resp
--- koukan/recipient_router_filter_test.py
+++ koukan/recipient_router_filter_test.py
@@ -12,67 +12,109 @@ from koukan.recipient_router_filter import (
 from koukan.filter import HostPort, Mailbox, TransactionMetadata
 from koukan.response import Response
 
-class SuccessPolicy(RoutingPolicy):
+class Policy(RoutingPolicy):
     def endpoint_for_rcpt(self, rcpt) -> Tuple[
             Optional[Destination], Optional[Response]]:
-        return Destination(
-            'http://localhost:8001', 'gateway',
-            [HostPort('example.com', 1234)]), None
+        if rcpt == 'good':
+            return Destination(
+                'http://localhost:8001', 'gateway',
+                [HostPort('example.com', 1234)]), None
+        if rcpt == 'bad':
+            return None, Response(500, 'not found')
 
-class FailurePolicy(RoutingPolicy):
-    def endpoint_for_rcpt(self, rcpt) -> Tuple[
-            Optional[Destination], Optional[Response]]:
-        return None, Response(500, 'not found')
-
-class RecipientRouterFilterTest(unittest.TestCase):
+class RecipientRouterFilterTest(unittest.IsolatedAsyncioTestCase):
     def setUp(self):
         logging.basicConfig(
             level=logging.DEBUG,
             format='%(asctime)s [%(thread)d] %(filename)s:%(lineno)d '
             '%(message)s')
 
-    def test_success(self):
-        router = RecipientRouterFilter(SuccessPolicy())
+    async def test_success(self):
+        router = RecipientRouterFilter(Policy())
         router.wire_downstream(TransactionMetadata())
+        router.wire_upstream(TransactionMetadata())
         tx = router.downstream_tx
 
-        delta = TransactionMetadata(
-            mail_from=Mailbox('alice'),
-            rcpt_to=[Mailbox('bob@domain')])
-
-        delta.body = InlineBlob(
+        prev = tx.copy()
+        tx.mail_from=Mailbox('alice')
+        tx.rcpt_to=[Mailbox('good')]
+        tx.body = InlineBlob(
             b'From: <alice>\r\n'
             b'To: <bob>\r\n'
             b'\r\n'
             b'hello\r\n')
-        tx.merge_from(delta)
+        delta = prev.delta(tx)
 
-        router.on_update(delta)
-        self.assertEqual(router.upstream_tx.rest_endpoint, 'http://localhost:8001')
+        async def upstream():
+            logging.debug(router.upstream_tx)
+            prev = router.upstream_tx.copy()
+            router.upstream_tx.mail_response = Response(201)
+            router.upstream_tx.rcpt_response = [Response(202)]
+            return prev.delta(router.upstream_tx)
+
+        await router.on_update(delta, upstream)
+        logging.debug(router.downstream_tx)
+        self.assertEqual(router.upstream_tx.rest_endpoint,
+                         'http://localhost:8001')
         self.assertEqual(router.upstream_tx.upstream_http_host, 'gateway')
         self.assertEqual(router.upstream_tx.resolution.hosts,
                          [HostPort('example.com', 1234)])
 
 
-    # TODO: exercise "buffer mail"
-
-    def test_failure(self):
-        router = RecipientRouterFilter(FailurePolicy())
+    async def test_failure(self):
+        router = RecipientRouterFilter(Policy())
         tx = TransactionMetadata()
         router.wire_downstream(tx)
+        router.wire_upstream(TransactionMetadata())
 
-        delta = TransactionMetadata(
-            mail_from=Mailbox('alice'),
-            rcpt_to=[Mailbox('bob@domain')],
-            body = InlineBlob(
-                b'From: <alice>\r\n'
-                b'To: <bob>\r\n'
-                b'\r\n'
-                b'hello\r\n'))
+        prev = tx.copy()
+        tx.mail_from=Mailbox('alice')
+        tx.rcpt_to=[Mailbox('bad')]
+        tx.body = InlineBlob(
+            b'From: <alice>\r\n'
+            b'To: <bob>\r\n'
+            b'\r\n'
+            b'hello\r\n')
+        delta = prev.delta(tx)
 
-        tx.merge_from(delta)
-        router.on_update(delta)
-        self.assertEqual([r.code for r in tx.rcpt_response], [500])
+        async def upstream():
+            prev = router.upstream_tx.copy()
+            router.upstream_tx.mail_response = Response(201)
+            return prev.delta(router.upstream_tx)
+
+        await router.on_update(delta, upstream)
+        self.assertEqual(201, tx.mail_response.code)
+        self.assertEqual([500], [r.code for r in tx.rcpt_response])
+
+
+    async def test_mixed(self):
+        router = RecipientRouterFilter(Policy())
+        tx = TransactionMetadata()
+        router.wire_downstream(tx)
+        router.wire_upstream(TransactionMetadata())
+
+        prev = tx.copy()
+        tx.mail_from=Mailbox('alice')
+        tx.rcpt_to=[Mailbox('bad'),
+                    Mailbox('good')]
+        tx.body = InlineBlob(
+            b'From: <alice>\r\n'
+            b'To: <bob>\r\n'
+            b'\r\n'
+            b'hello\r\n')
+        delta = prev.delta(tx)
+
+        async def upstream():
+            prev = router.upstream_tx.copy()
+            router.upstream_tx.mail_response = Response(201)
+            router.upstream_tx.rcpt_response = [Response(202)]
+            return prev.delta(router.upstream_tx)
+
+        await router.on_update(delta, upstream)
+        logging.debug(tx)
+        self.assertEqual(201, tx.mail_response.code)
+        self.assertEqual([500, 202], [r.code for r in tx.rcpt_response])
+
 
 
 if __name__ == '__main__':
