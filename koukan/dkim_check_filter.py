4c9172cb488c630e66eb9a8907b2774d39ed176d
John Bucy <bucy@gloop.org>
2026-02-05 17:29:19 -0800

    wip add dkim_check_filter based on dkimpy
    This verifies all dkim-signature headers in the message and populates
    the results into the filter output. This also populates most of the
    signature tags/fields into the filter output to make them available to
    custom predicates.
    
    The stock matcher/predicate supports:
    - verification result
      we break out unknown algo as a distinct result to try to be future-proof
      though this may be academic/moot
    - signing domain alignment: exact, same sld, other
    - mutually exclusive with alignment: exact domain for inbound gateway setups
    
    todo
    - test verify json
    - test verify failed signature
    - filter_chain_wiring

---
+++ koukan/dkim_check_filter.py
@@ -0,0 +1,158 @@
+# Copyright The Koukan Authors
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import Any, Dict, List, Optional
+from enum import IntEnum
+import logging
+
+import dkim
+import dkim.util
+import publicsuffix2
+
+from koukan.address import domain_from_address
+from koukan.filter import TransactionMetadata
+from koukan.filter_chain import Filter, FilterResult
+from koukan.filter_output import FilterOutput
+from koukan.matcher_result import MatcherResult
+from koukan.response import Response
+from koukan.rest_schema import WhichJson
+
+from koukan.message_validation_filter import MessageValidationFilter
+
+class Status(IntEnum):
+    temp_err = 0  # e.g. dns
+    dkim_pass = 1
+    fail = 2
+    unknown_algo = 3
+
+
+# header-from domain vs d= "sdid"
+class Alignment(IntEnum):
+    domain = 0
+    same_org = 1  # publicsuffix2.get_sld() a la dmarc relaxed alignment
+    other = 2
+
+class DkimCheckFilterOutput(FilterOutput):
+
+    # for each dkim-signature header
+    class Result:
+        domain : Optional[str] = None  # d= sdid
+        status : Status
+        alignment : Alignment
+        tags : Dict[bytes, bytes]
+        def __init__(self):
+            self.tags = {}
+
+    results: List[Result]
+
+    def __init__(self):
+        self.results = []
+
+    def match(self, yaml : dict):
+        # 1: any sig w/specified alignment passes
+        # 2: sig w/any alignment from known inbound-gateway domain
+        assert not ('alignment' in yaml and 'domains' in yaml)
+        align = Alignment[yaml.get('alignment', 'same_org')]
+        status = Status[yaml.get('status', 'dkim_pass')]
+        domains = yaml.get('domains', [])
+        for r in self.results:
+            if r.status != status:
+                continue
+            if domains:
+                for d in domains:
+                    if d == r.domain:  # xxx case?
+                        return MatcherResult.MATCH
+                return MatcherResult.NO_MATCH
+
+            # xxx inclusive same_org also matches domain?
+            if r.alignment != align:
+                continue
+            return MatcherResult.MATCH
+
+        return MatcherResult.NO_MATCH
+
+    def to_json(self, w : WhichJson):
+        if self.results is None:
+            return None
+        return {
+            'results': [
+                { 'status': r.status,
+                  'alignment': r.alignment,
+                  'signing_domain': r.domain,
+                  'tags': r.tags } for r in self.results
+            ]
+        }
+
+class DkimCheckFilter(Filter):
+    def __init__(self, inject_dns=None):
+        self.inject_dns = inject_dns
+
+    def on_update(self, tx_delta : TransactionMetadata) -> FilterResult:
+        if (body := tx_delta.maybe_body_blob()) is None or not body.finalized():
+            return FilterResult()
+
+        tx = self.downstream_tx
+        assert tx is not None
+
+        b = body.pread(0)
+        verifier = dkim.DKIM(b)
+
+        out = tx.get_filter_output(self.fullname())
+        if out is None:
+            out = DkimCheckFilterOutput()
+            tx.add_filter_output(self.fullname(), out)
+
+        i = 0
+        for k,v in verifier.headers:
+            if k.lower() != b'dkim-signature':
+                continue
+            ex = None
+            res = DkimCheckFilterOutput.Result()
+            signer_domain = None
+            try:
+                res.tags = {k.decode('ascii'):v.decode('ascii')
+                            for k,v in dkim.util.parse_tag_value(v).items()}
+                # xxx decode error, domain could be utf8?
+                res.domain = res.tags['d']
+                del res.tags['d']
+                # drop these because they're large and unlikely to be
+                # useful to matchers
+                del res.tags['b']
+                del res.tags['bh']
+                kwargs = {}
+                if self.inject_dns:
+                    kwargs['dnsfunc'] = self.inject_dns
+                s = verifier.verify(i, **kwargs)
+                status = Status.dkim_pass if s is True else Status.fail
+                logging.debug(status)
+            except dkim.ValidationError as e:
+                logging.debug(e)
+                ex = e
+                if 'unknown signature algorithm' in str(e):
+                    status = Status.unknown_algo
+                else:
+                    status = Status.fail
+            except Exception as e:
+                logging.debug(e)
+                ex = e
+                status = Status.temp_err
+
+            valid = tx.get_filter_output(MessageValidationFilter.fullname())
+            header_from_domain = valid.parsed_header_from.domain
+            logging.debug('%s %s', header_from_domain, res.domain)
+            if header_from_domain == res.domain:  # xxx case?
+                res.alignment = Alignment.domain
+            else:
+                header_from_sld = publicsuffix2.get_sld(header_from_domain)
+                signer_sld = publicsuffix2.get_sld(res.domain)
+                logging.debug('%s %s', header_from_sld, signer_sld)
+                if header_from_sld == signer_sld:  # xxx case?
+                    res.alignment = Alignment.same_org
+                else:
+                    res.alignment = Alignment.other
+
+            res.status = status
+            out.results.append(res)
+            i += 1
+
+        return FilterResult()
---
+++ koukan/dkim_check_filter_test.py
@@ -0,0 +1,134 @@
+# Copyright The Koukan Authors
+# SPDX-License-Identifier: Apache-2.0
+
+import logging
+import unittest
+import tempfile
+
+import socket
+from email.headerregistry import Address, AddressHeader
+
+from dkim import dknewkey, sign
+
+
+from koukan.filter import HostPort, Mailbox, TransactionMetadata
+from koukan.dkim_check_filter import (
+    Alignment,
+    DkimCheckFilter,
+    DkimCheckFilterOutput,
+    Status )
+from koukan.matcher_result import MatcherResult
+from koukan.rest_schema import WhichJson
+
+from koukan.blob import InlineBlob
+
+from koukan.message_validation_filter import (
+    MessageValidationFilter,
+    MessageValidationFilterResult )
+
+
+class DkimCheckFilterTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.tempdir = tempfile.TemporaryDirectory()
+        dir = cls.tempdir.name
+        cls.privkey_filename = dir + '/privkey'
+        cls.pubkey_filename = dir + '/pubkey'
+        dknewkey.GenRSAKeys(cls.privkey_filename)
+        dknewkey.ExtractRSADnsPublicKey(cls.privkey_filename, cls.pubkey_filename)
+        with open(cls.privkey_filename, 'rb') as priv:
+            cls.privkey = priv.read()
+        with open(cls.pubkey_filename, 'rb') as pub:
+            cls.pubkey = pub.read()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.tempdir.cleanup()
+
+    def setUp(self):
+        self.maxDiff = 4096
+
+    def dns(self, name, timeout):
+        logging.debug(name)
+        if name == b'my-selector._domainkey.example.com.':
+            return self.pubkey
+        if name == b'my-selector._domainkey.lists.example.com.':
+            return self.pubkey
+        if name == b'my-selector._domainkey.somewhere-else.com.':
+            return self.pubkey
+        return None
+
+    def test_smoke(self):
+        with open('testdata/trivial.msg', 'rb') as message_file:
+            message = message_file.read()
+
+        sig = sign(message, b'my-selector', b'example.com', self.privkey)
+        sig = sig.replace(b'a=rsa-sha256;', b'a=rsa-sha1024;')
+        message = sig + message
+
+        for domain in [b'lists.example.com', b'example.com', b'somewhere-else.com']:
+            sig = sign(message, b'my-selector', domain, self.privkey)
+            message = sig + message
+
+        logging.debug(message)
+
+        valid = MessageValidationFilterResult()
+        valid.parsed_header_from = Address(addr_spec='alice@example.com')
+
+        f = DkimCheckFilter(self.dns)
+        tx = TransactionMetadata()
+        f.wire_downstream(tx)
+        prev = tx.copy()
+        tx.mail_from = Mailbox('alice@example.com')
+        tx.body = InlineBlob(message, last=True)
+        tx.add_filter_output(MessageValidationFilter.fullname(), valid)
+        f.on_update(prev.delta(tx))
+
+        out = tx.get_filter_output(f.fullname())
+        self.assertEqual(4, len(out.results))
+        r0 = out.results[1]
+        self.assertEqual(Status.dkim_pass, r0.status)
+        self.assertEqual(Alignment.domain, r0.alignment)
+        self.assertEqual('example.com', r0.domain)
+
+        r1 = out.results[2]
+        self.assertEqual(Status.dkim_pass, r1.status)
+        self.assertEqual(Alignment.same_org, r1.alignment)
+        self.assertEqual('lists.example.com', r1.domain)
+
+        r2 = out.results[3]
+        self.assertEqual(Status.unknown_algo, r2.status)
+
+        r3 = out.results[0]
+        self.assertEqual(Status.dkim_pass, r3.status)
+        self.assertEqual(Alignment.other, r3.alignment)
+        self.assertEqual('somewhere-else.com', r3.domain)
+
+        logging.debug(out.to_json(WhichJson.DB_ATTEMPT))
+
+        self.assertEqual(
+            MatcherResult.MATCH,
+            out.match({'alignment': 'domain',
+                       'status': 'dkim_pass'}))
+        self.assertEqual(
+            MatcherResult.NO_MATCH,
+            out.match({'alignment': 'domain',
+                       'status': 'temp_err'}))
+
+        self.assertEqual(
+            MatcherResult.MATCH,
+            out.match({'status': 'dkim_pass',
+                       'domains': ['somewhere-else.com']}))
+        self.assertEqual(
+            MatcherResult.NO_MATCH,
+            out.match({'status': 'dkim_pass',
+                       'domains': ['somewhere-else.org']}))
+
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s [%(thread)d] %(filename)s:%(lineno)d %(message)s')
+
+    unittest.main()
