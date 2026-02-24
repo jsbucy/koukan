d5d730ce5c7c59306abc37248903c9139cb0699e
John Bucy <bucy@gloop.org>
2026-02-24 14:02:41 -0800

    matchers assert preconditions
    These asserts are of the form "smtp_meta must be present by the time
    we get mail_from" which is a little more flexible than hard-coding
    "smtp_meta must be present in the first update"

--- koukan/transaction_matchers.py
+++ koukan/transaction_matchers.py
@@ -9,6 +9,7 @@ from koukan.filter import TransactionMetadata
 # Kitchen sink for trivial matchers
 
 def match_network_address(yaml : dict, tx : TransactionMetadata):
+    assert tx.mail_from is None or tx.remote_host is not None
     if tx.remote_host is None:
         return MatcherResult.PRECONDITION_UNMET
     ip = ip_address(tx.remote_host.host)
@@ -16,11 +17,13 @@ def match_network_address(yaml : dict, tx : TransactionMetadata):
     return MatcherResult.from_bool(ip in cidr)
 
 def match_tls(yaml : dict, tx : TransactionMetadata):
+    assert tx.mail_from is None or tx.smtp_meta is not None
     if tx.smtp_meta is None:
         return MatcherResult.PRECONDITION_UNMET
     return MatcherResult.from_bool(tx.smtp_meta.get('tls', False))
 
 def match_smtp_auth(yaml : dict, tx : TransactionMetadata):
+    assert tx.mail_from is None or tx.smtp_meta is not None
     if tx.smtp_meta is None:
         return MatcherResult.PRECONDITION_UNMET
     return MatcherResult.from_bool(tx.smtp_meta.get('auth', False))
